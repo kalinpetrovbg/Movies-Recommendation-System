f0e16f04cbec13a64fa57581cfa41a2db21facc2
Kalin Petrov <kalinpetrovbg@gmail.com>
2024-10-03 16:45:53 +0300

    adding collaborative endpoint (basis version)

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from starlette.responses import HTMLResponse
+
+from collaborative_based import CollaborativeBased
 from popularity_based import PriorityBased
 import uvicorn
-from models.models import Movie
+from models.models import Movie, CollaborativeModel
 
 movies = PriorityBased("data/movies.csv")
 movies.load_data()
 movies.calculate_weighted_ratings()
+
+collaboratives = CollaborativeBased("data/movies.csv")  # Todo
 
 templates = Jinja2Templates(directory="templates")
 app = FastAPI()
@@ -18,7 +22,9 @@ async def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
 
-@app.get("/popularity", response_class=HTMLResponse, tags=["view"])
+@app.get(
+    "/popularity", response_class=HTMLResponse, tags=["view"], include_in_schema=False
+)
 async def popularity(request: Request):
     movies_data = movies.get_movies_data()
     return templates.TemplateResponse(
@@ -31,19 +37,39 @@ async def popularity_api():
     return movies.get_movies_data()
 
 
-@app.get("/collaborative", response_class=HTMLResponse, deprecated=True, tags=["view"])
-async def collaborative(request: Request):
-    pass
+@app.get("/collaborative/{movie_name}", tags=["view"], include_in_schema=False)
+async def collaborative(request: Request, movie_name: str):
+    try:
+        movies_data = collaboratives.get_movies_data(movie_name, number_of_movies=10)
+    except IndexError:
+        raise HTTPException(status_code=404, detail=f"Movie '{movie_name}' not found.")
+    return templates.TemplateResponse(
+        "collaborative.html", {"request": request, "movies": movies_data}
+    )
 
 
 @app.get(
-    "/api/collaborative", response_model=list[Movie], deprecated=True, tags=["api"]
+    "/api/collaborative/{movie_name}",
+    response_model=list[CollaborativeModel],
+    tags=["api"],
 )
-async def collaborative_api():
-    return []
+async def collaborative_api(movie_name: str):
+    try:
+        coll_movies = collaboratives.get_movies_api_data(
+            movie_name, number_of_movies=10
+        )
+    except IndexError:
+        raise HTTPException(status_code=404, detail=f"Movie '{movie_name}' not found.")
+    return [CollaborativeModel(**movie) for movie in coll_movies]
 
 
-@app.get("/content", response_class=HTMLResponse, deprecated=True, tags=["view"])
+@app.get(
+    "/content",
+    response_class=HTMLResponse,
+    deprecated=True,
+    tags=["view"],
+    include_in_schema=False,
+)
 async def content(request: Request):
     pass
 
--- models/models.py
+++ models/models.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
 
 class Movie(BaseModel):
+    id: int
     title: str
     vote_count: int
     vote_average: float
     weighted_rating: float
+
+class CollaborativeModel(BaseModel):
+    id: int
+    title: str
