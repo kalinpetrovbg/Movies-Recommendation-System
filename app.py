87d0a8dd2614c44c95a250189ca925b9a546d98d
Kalin Petrov <kalinpetrovbg@gmail.com>
2024-10-08 19:28:57 +0300

    remove logging

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-# app.py
-
-import logging
-
 import uvicorn
 from fastapi import FastAPI
 
 from endpoints.api import router as api_router
 from endpoints.views import router as views_router
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
