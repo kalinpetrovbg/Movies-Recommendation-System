6df902f8d2b93032a05f4b4b772e265f230c2c73
Kalin Petrov <kalinpetrovbg@gmail.com>
2024-10-08 19:03:21 +0300

    fix the logic with the missing movies

--- algorithms/collaborative_based.py
+++ algorithms/collaborative_based.py
@@ -3,13 +3,25 @@ from surprise import SVD, Dataset, Reader
 
 
 class CollaborativeBased:
-    def __init__(self, ratings_file):
+    def __init__(self, ratings_file, movies_file):
         self.ratings = pd.read_csv(ratings_file)
+        self.movies = pd.read_csv(movies_file)
+
+        # Check for missing movies and filter ratings
+        if not self.movies.empty:
+            rated_movie_ids = set(self.ratings['movieId'].unique())
+            existing_movie_ids = set(self.movies['id'].unique())
+            missing_movie_ids = rated_movie_ids - existing_movie_ids
+
+            if missing_movie_ids:
+                self.ratings = self.ratings[
+                    ~self.ratings['movieId'].isin(missing_movie_ids)]
+
         reader = Reader(rating_scale=(1, 5))
-        data = Dataset.load_from_df(
+        dataset = Dataset.load_from_df(
             self.ratings[["userId", "movieId", "rating"]], reader
         )
-        self.trainset = data.build_full_trainset()
+        self.trainset = dataset.build_full_trainset()
 
         # Train the SVD algorithm
         self.svd = SVD()
@@ -17,18 +29,19 @@ class CollaborativeBased:
 
     def get_recommendations(self, user_id, top_n=6):
         if not self.trainset.knows_user(user_id):
-            raise ValueError(f"User ID {user_id} does not exist in the dataset")
+            raise ValueError(
+                f"User ID {user_id} does not exist in the dataset")
 
-        # Predict ratings for all movies that the user hasn't rated yet
         rated_movies = self.ratings[self.ratings["userId"] == user_id][
-            "movieId"
-        ].tolist()
-        all_movies = self.ratings["movieId"].unique()
-        unrated_movies = [movie for movie in all_movies if movie not in rated_movies]
+            "movieId"].tolist()
 
-        predictions = [
-            self.svd.predict(user_id, movie_id) for movie_id in unrated_movies
-        ]
+        all_movies = self.ratings["movieId"].unique()
+        unrated_movies = [movie for movie in all_movies if
+                          movie not in rated_movies]
+
+        # Predict ratings for all unrated movies
+        predictions = [self.svd.predict(user_id, movie_id) for movie_id in
+                       unrated_movies]
         predictions.sort(key=lambda x: x.est, reverse=True)
 
         # Get the top N recommendations
--- dependencies.py
+++ dependencies.py
@@ -11,7 +11,7 @@ movies_csv.data["id"] = movies_csv.data["id"].astype(int)
 
 priority_data = PriorityBased(movies_csv.data)
 content_data = ContentBased(movies_csv.data)
-collaborative_data = CollaborativeBased("data/ratings.csv")
+collaborative_data = CollaborativeBased("data/ratings.csv", "data/movies.csv")
 
 movie_id_to_title = dict(
     zip(movies_csv.data["id"], movies_csv.data["title"])
